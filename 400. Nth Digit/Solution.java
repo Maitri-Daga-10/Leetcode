2f247245fb69e37522beafa0bcc1d7963ad63c8b
Maitri daga <maitri.daga.10@gmail.com>
2026-07-03 22:11:24 +0530

    400. Nth Digit

---
+++ 400. Nth Digit/Solution.java
@@ -0,0 +1,16 @@
+class Solution{
+    public int findNthDigit(int n){
+        long digitLength = 1;
+        long count = 9;
+        long start = 1;
+        while (n > digitLength * count){
+            n -= digitLength * count;
+            digitLength++;
+            count *= 10;
+            start *= 10;
+        }
+        long number = start + (n - 1) / digitLength;
+        String s = String.valueOf(number);
+        return s.charAt((n - 1) % (int) digitLength) - '0';
+    }
+}
