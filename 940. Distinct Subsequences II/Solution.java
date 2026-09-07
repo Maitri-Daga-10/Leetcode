e9075a78d84d160d275122901892bbd474873318
Maitri daga <maitri.daga.10@gmail.com>
2026-09-08 00:00:15 +0530

    940. Distinct Subsequences II

---
+++ 940. Distinct Subsequences II/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int distinctSubseqII(String s) {
+        int MOD = 1000000007;
+        long[] last = new long[26];
+        long total = 0;
+        for (char ch : s.toCharArray()){
+            int index = ch - 'a';
+            long newSubsequences = (total + 1) % MOD;
+            total = (total + newSubsequences - last[index] + MOD) % MOD;
+            last[index] = newSubsequences;
+        }
+        return (int) total;
+    }
+}
